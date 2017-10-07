aeb422d8d461661043e709e7d74a2fb6f622d261
jy95 <jacques.yakoub@gmail.com>
2017-10-07 16:16:03 +0200

    real_address amélioré
    real_address : utilisation de gai_strerror pour avoir un message plus clair.
    
    Init du folder src , pour suivre les conventions du projet

--- src/stepConnexion.c
+++ src/stepConnexion.c
@@ -1,7 +1,7 @@
 #include "headers/create_socket.h"
 #include "headers/real_address.h"
 
-#include <netdb.h> // Voir si nécessaire ; sur INGI, ne passait pas sans
+#include <netdb.h> // Nécessaire
 #include <stdio.h>
 #include <string.h>
 
@@ -14,19 +14,18 @@ const char *real_address(const char *address, struct sockaddr_in6 *rval) {
 
     // settage des paramètres
     options.ai_family = AF_INET6; // IPV6
-    options.ai_socktype = SOCK_STREAM;
+    options.ai_socktype = SOCK_DGRAM; // SOCK_DGRAM ou SOCK_STREAM
     options.ai_flags = 0;
-    options.ai_protocol = 0;          /* N'importe quel protocol */
+    //options.ai_protocol = 0;          /* N'importe quel protocol (pas nécessaire) */
 
     // on essaye de trouver une adresse
     // service à NULL parce qu'on a pas un numéro de port précis
-    const char *errorMessage = "CANNOT FIND THE IPV6 ADRESSE";
 
     errorCode = getaddrinfo(address, NULL, &options, &result);
     if(errorCode != 0) {
         // On a un problème
-        fprintf(stderr, "ERROR CODE : %d \n", errorCode);
-        return errorMessage;
+        // gai_strerror nous donne une explication concrète avec le errorCode
+        return gai_strerror(errorCode);
     } else {
         // en théorie , la structure addrinfo est récursive
         // nous, nous n'avons besoin que de la première adresse IPv6 qu'il a trouvé
@@ -40,13 +39,13 @@ const char *real_address(const char *address, struct sockaddr_in6 *rval) {
         // et que c'est de IPV6
         rval->sin6_family = AF_INET6;
 
+        // on libère la ressource temporaire
         freeaddrinfo(result);
 
         return NULL;
     }
 }
 
-// TODO : Code non testé
 int create_socket(struct sockaddr_in6 *source_addr,
                   int src_port,
                   struct sockaddr_in6 *dest_addr,
@@ -55,8 +54,8 @@ int create_socket(struct sockaddr_in6 *source_addr,
 
     // Etape 1 : ouverture du socket
     int socketFileDescriptor;
-
-    if ( ( socketFileDescriptor = socket(AF_INET6,SOCK_STREAM,0) ) == -1) {
+    // SOCK_DGRAM ou SOCK_STREAM pour le 3e param
+    if ( ( socketFileDescriptor = socket(AF_INET6,SOCK_DGRAM,0) ) == -1) {
         // pas possible d'ouvrir le socket
         fprintf(stderr,"Cannot create socket\n");
         return -1;
@@ -87,7 +86,7 @@ int create_socket(struct sockaddr_in6 *source_addr,
         } else {
             dest_addr->sin6_port = 0; // par défaut, n'importe quel port suffira
         }
-
+        // Bloque ici sur INGI
         if ( connect(socketFileDescriptor, (struct sockaddr *) dest_addr, sizeof(*dest_addr) ) == -1 ){
             fprintf(stderr,"Cannot connect with destination address\n");
             return -1;
