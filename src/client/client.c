1c26e9784b8e7ecaec071013e3e2ebcd3d726870
jy95 <jacques.yakoub@gmail.com>
2017-10-15 02:33:05 +0200

    Proposition pour le RTT initial
    Un moyen d'obtenir un RTT initial.
    Par la suite, il suffira de faire en théorie
    
    RTT = ( T2+T1 ) / 2 ;

--- src/client/client.c
+++ src/client/client.c
@@ -1,10 +1,11 @@
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
 #include "../sendAndReceiveData/create_socket.h"
 #include "../sendAndReceiveData/real_address.h"
 #include "../sendAndReceiveData/read_write_loop.h"
@@ -14,14 +15,16 @@ int main(int argc, char *argv[]) {
     option_t *option_arg = get_option_arg(argc, argv); //On ajoute dans la structure les infos de la ligne de commande
     if ( option_arg == NULL) return EXIT_FAILURE;
 
+    FILE *fp = NULL;
+
+    // Redirection depuis le fichier
+    // https://www.tutorialspoint.com/c_standard_library/c_function_freopen.htm
+    // exemple : https://stackoverflow.com/a/586490/6149867
     if ( option_arg->filename != NULL ) {
-        FILE *fp;
-        if ((fp = fopen(option_arg->filename, "r+")) == NULL ) {
+        if ((fp = freopen(option_arg->filename, "r", stdin)) == NULL ) {
             fprintf(stderr, "Le fichier ne peut pas être lue\n");
             return EXIT_FAILURE;
         }
-        // maintenant ce fichier doit devenir l'entrée standard
-        // dup2 avec STDIN
     }
 
     struct sockaddr_in6 rval;
@@ -37,19 +40,33 @@ int main(int argc, char *argv[]) {
     inet_ntop(AF_INET6, &(rval.sin6_addr), ipAddress, INET6_ADDRSTRLEN);
     fprintf(stdout, "Use IPV6 Address: %s\n", ipAddress);
 
+    // Step : Calculer le RTT initial pour envoyer un packet (en théorie, doit être recalculé à chaque réception de (N)ACK)
+    // Pour cela, on va l'estimer à l'aide de MAX_LATENCE_TIME et EXPECTED_LATENCE_RATE
+    struct timeval RTT;
+
     int socketFileDescriptor;
+
+    // on init le temps supposé de latence
+    int delay = MAX_LATENCE_TIME / EXPECTED_LATENCE_RATE;
+    // ms -> s : 1/1000
+    RTT.tv_sec = (delay / 1000);
+    // us -> s : 1000
+    RTT.tv_usec = (delay - (RTT.tv_sec * 1000)) * 1000;
+
     if ((socketFileDescriptor = create_socket(NULL, -1, &rval, option_arg->port)) < 0 )
         return EXIT_FAILURE; //On connecte le client au serveur
 
     fprintf(stdout, "Socket successfully created - listenning to port %d\n", option_arg->port);
-
-    // Step : Calculer le RTT moyen pour envoyer un packet
-    // Algo de base : envoyer 3 paquet tronqués hors séquence pour savoir
-
+    fprintf(stdout, "Initial calculated RTT : %d s - %d ms \n", (int) RTT.tv_sec, (int) (RTT.tv_usec / 1000));
 
     // Step : Envoi de message
 
     read_write_loop(socketFileDescriptor);
+
+    // si on a ouvert le fp
+    if ( fp ) {
+        fclose(fp);
+    }
 
     return EXIT_SUCCESS;
 }
--- src/client/client.h
+++ src/client/client.h
@@ -5,6 +5,10 @@
 #ifndef PROJECT_CLIENT_H
 #define PROJECT_CLIENT_H
 
+#define MAX_LATENCE_TIME    2000 // le temps maximal
+#define EXPECTED_LATENCE_RATE  10   // un pourcentage arbitraire pour évaluer délai/latence/le temps de traitement
+#define MAX_PAYLOAD_SIZE    512  // la taille MAX du payload
+
 typedef struct option_arg { //Regroupe trois type d'argument possibles
     const char *filename; //le nom de fichier avec l'option -f
     const char *domaine; // le nom de domain ou l'adresse IPV6
